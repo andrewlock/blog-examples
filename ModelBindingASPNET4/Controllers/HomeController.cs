f63c1fbacfec14b56c3da3d79d3a7a9ed79124ce
Sock <andrew.lock@stormfront.co.uk>
2016-05-12 18:49:54 +0100

    Add POST via postback, form encoded and json

--- ModelBindingASPNET4/Controllers/HomeController.cs
+++ ModelBindingASPNET4/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ModelBindingASPNET4.Models;
 
 namespace ModelBindingASPNET4.Controllers
 {
@@ -11,6 +12,18 @@ namespace ModelBindingASPNET4.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Person person)
+        {
+            return View(person);
+        }
+
+        [HttpPost]
+        public ActionResult IndexApi(Person person)
+        {
+            return Json(person);
         }
 
         public ActionResult About()
