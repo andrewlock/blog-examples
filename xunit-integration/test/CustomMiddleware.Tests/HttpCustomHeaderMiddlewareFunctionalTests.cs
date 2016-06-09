71f0e57c49ce12c0c8803a8f1e4ab192264f4b11
Sock <andrew.lock@stormfront.co.uk>
2016-06-09 21:01:10 +0100

    Added xunit integration tests project

---
+++ xunit-integration/test/CustomMiddleware.Tests/HttpCustomHeaderMiddlewareFunctionalTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CustomMiddleware.Tests
+{
+    public class HttpCustomHeaderMiddlewareFunctionalTests : IClassFixture<HttpCustomHeaderFixture<DemoWebsite.Startup>>
+    {
+        public HttpCustomHeaderMiddlewareFunctionalTests(HttpCustomHeaderFixture<DemoWebsite.Startup> fixture)
+        {
+            Client = fixture.Client;
+        }
+
+        public HttpClient Client { get; }
+
+        [Theory]
+        [InlineData("GET")]
+        [InlineData("HEAD")]
+        [InlineData("POST")]
+        [InlineData("PUT")]
+        public async Task AllMethods_AddCustomHeader(string method)
+        {
+            // Arrange
+            var path = "/CustomHeadersMiddleware/BG36A632-C4D2-4B71-B2BD-18625ADDA87F";
+            var request = new HttpRequestMessage(new HttpMethod(method), path);
+
+            // Act
+            var response = await Client.SendAsync(request);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            var content = await response.Content.ReadAsStringAsync();
+            Assert.Equal(path, content);
+            var responseHeaders = response.Headers;
+
+            var header = response.Headers.GetValues("X-Demo-Header").FirstOrDefault();
+            Assert.Equal(header, "Header custom value");
+
+        }
+    }
+}
