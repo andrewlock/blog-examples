d3671516fc930e01ea7909bd5b8404fd8dad21ae
Sock <andrew.lock@stormfront.co.uk>
2016-07-14 19:11:08 +0100

    Add DbContext and configure app for postregsql

--- db-multi-tenancy-in-saaskit/src/DatabaseMultiTenancyWithSaasKit/Startup.cs
+++ db-multi-tenancy-in-saaskit/src/DatabaseMultiTenancyWithSaasKit/Startup.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DatabaseMultiTenancyWithSaasKit.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -29,6 +31,12 @@ namespace DatabaseMultiTenancyWithSaasKit
         {
             // Add framework services.
             services.AddMvc();
+
+
+            var connectionString = Configuration["ApplicationDbContext:ConnectionString"];
+            services.AddDbContext<ApplicationDbContext>(
+                opts => opts.UseNpgsql(connectionString)
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
