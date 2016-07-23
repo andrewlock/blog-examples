94bbaf46da873436fddfb92d9bad8e7f4a6b8a52
Sock <andrew.lock@stormfront.co.uk>
2016-07-23 18:15:54 +0100

    Add per tenant static files handler

--- tenant-specific-static-files-in-saaskit/src/DatabaseMultiTenancyWithSaasKit/Startup.cs
+++ tenant-specific-static-files-in-saaskit/src/DatabaseMultiTenancyWithSaasKit/Startup.cs
@@ -6,6 +6,7 @@ using DatabaseMultiTenancyWithSaasKit.Models;
 using DatabaseMultiTenancyWithSaasKit.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -53,10 +54,52 @@ namespace DatabaseMultiTenancyWithSaasKit
                 app.UseExceptionHandler("/Home/Error");
             }
 
-            app.UseStaticFiles();
-            
             app.UseMultitenancy<AppTenant>();
-            
+
+            app.UsePerTenant<AppTenant>((ctx, builder) =>
+            {
+                bool isTenantSpecificPath = false;
+                const string prefix = "/tenant";
+                builder.Use(next =>
+                {
+                    return async httpContext =>
+                    {
+                        var originalPath = httpContext.Request.Path;
+                        isTenantSpecificPath = httpContext.Request.Path.StartsWithSegments(new PathString(prefix));
+                        if (isTenantSpecificPath)
+                        {
+                            var previousPath = httpContext.Request.Path.ToUriComponent()
+                                .Substring(prefix.Length);
+
+                            var newPath = new PathString($"/tenant/{ctx.Tenant.Tag}{previousPath}");
+                            httpContext.Request.Path = newPath;
+                        }
+                        await next(httpContext);
+
+                        //replace the original url after the remaing middleware has finished processing
+                        httpContext.Request.Path = originalPath;
+                    };
+                });
+                //render static files
+                builder.UseStaticFiles();
+                builder.Use(next =>
+                {
+                    return async httpContext =>
+                    {
+                        if (isTenantSpecificPath)
+                        {
+                            httpContext.Response.StatusCode = 404;
+                        }
+                        else
+                        {
+                            await next(httpContext);
+                        }
+                    };
+                });
+            });
+
+
+
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
