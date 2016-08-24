f8f42d81117ff3a487dfc8332847dcde2586b5e0
Sock <sock@andrewlock.net>
2016-08-24 21:00:47 +0100

    Initial commit

---
+++ using-session-state/src/UsingSessionState/ViewModels/SessionStateViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UsingSessionState.ViewModels
+{
+    public class SessionStateViewModel
+    {
+        public DateTime DateSessionStarted { get; set; }
+        public DateTime Now { get; set; }
+    }
+}
