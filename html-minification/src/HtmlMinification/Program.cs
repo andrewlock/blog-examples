21074c4ec896c00464a515641b8fb98b3805664b
Sock <sock@andrewlock.net>
2016-09-05 21:33:25 +0100

    Add command line args for setting environment

--- html-minification/src/HtmlMinification/Program.cs
+++ html-minification/src/HtmlMinification/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace HtmlMinification
 {
@@ -11,7 +12,12 @@ namespace HtmlMinification
     {
         public static void Main(string[] args)
         {
+            var config = new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .Build();
+
             var host = new WebHostBuilder()
+                .UseConfiguration(config)
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
