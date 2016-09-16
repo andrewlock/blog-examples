f1b9996bc90844e7307f8808b7ceb8c2e2c08b67
Sock <sock@andrewlock.net>
2016-09-16 22:06:24 +0100

    Add culture switcher partial

--- adding-localization/AddingLocalization/src/AddingLocalization/Controllers/HomeController.cs
+++ adding-localization/AddingLocalization/src/AddingLocalization/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AddingLocalization.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AddingLocalization.Controllers
@@ -42,6 +44,18 @@ namespace AddingLocalization.Controllers
         public IActionResult Error()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult SetLanguage(string culture, string returnUrl)
+        {
+            Response.Cookies.Append(
+                CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+                new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) }
+            );
+
+            return LocalRedirect(returnUrl);
         }
     }
 }
