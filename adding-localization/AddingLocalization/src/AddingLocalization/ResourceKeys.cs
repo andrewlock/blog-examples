8586f75f8191b354f5c4d4008bd20280b7c16ddb
Sock <sock@andrewlock.net>
2016-09-18 19:34:08 +0100

    Add additional resource key

--- adding-localization/AddingLocalization/src/AddingLocalization/ResourceKeys.cs
+++ adding-localization/AddingLocalization/src/AddingLocalization/ResourceKeys.cs
@@ -6,6 +6,7 @@ namespace AddingLocalization
 {
     public static class ResourceKeys
     {
+        public const string HomePage = "HomePage";
         public const string Required = "Required";
         public const string NotAValidEmail = "NotAValidEmail";
         public const string YourEmail = "YourEmail";
