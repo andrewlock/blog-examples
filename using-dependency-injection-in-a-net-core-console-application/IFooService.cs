5a2b5431442d4b20899eee37f6fba13eccac009e
Sock <sock@andrewlock.net>
2016-09-22 21:26:46 +0100

    Add logging and service collections to Console app

---
+++ using-dependency-injection-in-a-net-core-console-application/IFooService.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    public interface IFooService
+    {
+        void DoThing(int number);
+    }
+}
---
+++ using-dependency-injection-in-a-net-core-console-application/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using Microsoft.Extensions.Logging;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var serviceProvider = new ServiceCollection()
+                .AddLogging()
+                .AddSingleton<IFooService, FooService>()
+                .AddSingleton<BarService>()
+                .BuildServiceProvider();
+
+            //configure console logging
+            serviceProvider
+                .GetService<ILoggerFactory>()
+                .AddConsole(LogLevel.Debug);
+
+            var logger = serviceProvider.GetService<ILogger<Program>>();
+
+            logger.LogDebug("Starting application");
+
+            //do the hard work here
+            var bar = serviceProvider.GetService<BarService>();
+
+            bar.DoSomeRealWork();
+
+            logger.LogDebug("All done!");
+
+        }
+    }
+}
