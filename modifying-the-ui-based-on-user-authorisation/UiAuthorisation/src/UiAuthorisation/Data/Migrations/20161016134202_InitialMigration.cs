218acaf57724c461d9651fb859db9267440f95f8
Sock <sock@andrewlock.net>
2016-10-16 14:44:29 +0100

    Add initial application with identity and postgres

---
+++ modifying-the-ui-based-on-user-authorisation/UiAuthorisation/src/UiAuthorisation/Data/Migrations/20161016134202_InitialMigration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UiAuthorisation.Data.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
