fb4258c0d2704e954d474cf0c2e44f7b6d7d69c6
Sock <sock@andrewlock.net>
2016-10-25 20:42:12 +0100

    Initial commit

---
+++ injecting-services-when-creating-mvcoptions/src/Program.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+
+namespace injecting_services_when_creating_mvcoptions
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
