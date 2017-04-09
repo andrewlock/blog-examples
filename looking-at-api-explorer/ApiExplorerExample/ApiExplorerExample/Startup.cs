0af461b9839874a543b2a906dab395dd5c497d94
Andrew Lock <andrewlock.net@gmail.com>
2017-04-09 21:02:20 +0100

    Add initial api explorer

---
+++ looking-at-api-explorer/ApiExplorerExample/ApiExplorerExample/Startup.cs
@@ -0,0 +1,47 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.IO;
+using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace ApiExplorerWebSite
+{
+    public class Startup
+    {
+        // Set up application services
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<ILoggerFactory, LoggerFactory>();
+            services.AddMvc(options =>
+            {
+                options.Filters.AddService(typeof(ApiExplorerDataFilter));
+
+                options.Conventions.Add(new ApiExplorerVisibilityEnabledConvention());
+                options.Conventions.Add(new ApiExplorerVisibilityDisabledConvention(
+                    typeof(ApiExplorerVisbilityDisabledByConventionController)));
+
+                var jsonOutputFormatter = options.OutputFormatters.OfType<JsonOutputFormatter>().First();
+
+                options.OutputFormatters.Clear();
+                options.OutputFormatters.Add(jsonOutputFormatter);
+            });
+
+            services.AddSingleton<ApiExplorerDataFilter>();
+        }
+
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute("default", "{controller}/{action}");
+            });
+        }
+    }
+}
+
