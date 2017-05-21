a2c0e637ef290ea30122579ecac5f2b387a57a6d
Andrew Lock <andrewlock.net@gmail.com>
2017-05-21 17:40:16 +0100

    Razor pages conversion

---
+++ RazorPages/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RazorPages.Models;
+
+namespace RazorPages.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
