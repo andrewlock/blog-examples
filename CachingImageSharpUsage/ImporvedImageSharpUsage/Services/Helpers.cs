0b237bb9bcc432a63c4e997cadb3df795244ccbe
Andrew Lock <andrewlock.net@gmail.com>
2017-05-21 17:49:39 +0100

    Add caching method

---
+++ CachingImageSharpUsage/ImporvedImageSharpUsage/Services/Helpers.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.FileProviders;
+
+namespace ImporvedImageSharpUsage.Services
+{
+    public class Helpers
+    {
+        internal static IFileProvider ResolveFileProvider(IHostingEnvironment hostingEnv)
+        {
+            if (hostingEnv.WebRootFileProvider == null)
+            {
+                throw new InvalidOperationException("Missing FileProvider.");
+            }
+            return hostingEnv.WebRootFileProvider;
+        }
+
+        internal static bool PathEndsInSlash(PathString path)
+        {
+            return path.Value.EndsWith("/", StringComparison.Ordinal);
+        }
+
+        internal static bool TryMatchPath(PathString path, PathString matchUrl, bool forDirectory, out PathString subpath)
+        {
+            if (forDirectory && !PathEndsInSlash(path))
+            {
+                path += new PathString("/");
+            }
+
+            if (path.StartsWithSegments(matchUrl, out subpath))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
