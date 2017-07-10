d42fc1e67aaa26ca1064f86b0eefbb0934225ce3
Andrew Lock <andrewlock.net@gmail.com>
2017-07-10 19:13:51 +0100

    Added source code for https://andrewlock.net/creating-custom-password-validators-for-asp-net-core-identity

---
+++ creating-custom-password-validators-for-asp-net-core-identity/WebApplication18/UsernameAsPasswordValidator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using WebApplication18.Models;
+
+namespace WebApplication18
+{
+    public class UsernameAsPasswordValidator : IPasswordValidator<ApplicationUser> 
+    {
+        public Task<IdentityResult> ValidateAsync(UserManager<ApplicationUser> manager, ApplicationUser user, string password)
+        {
+            if (string.Equals(user.UserName, password, StringComparison.OrdinalIgnoreCase))
+            {
+                return Task.FromResult(IdentityResult.Failed(new IdentityError
+                {
+                    Code = "UsernameAsPassword",
+                    Description = "You cannot use your username as your password"
+                }));
+            }
+            return Task.FromResult(IdentityResult.Success);
+        }
+    }
+}
