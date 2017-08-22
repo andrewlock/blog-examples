6acfbab4b96d98374196f8949dc373c6d8fd9178
Andrew Lock <andrewlock.net@gmail.com>
2017-08-22 19:29:22 +0100

    Add cancellatino tokens example

---
+++ CancellationTokens/CancellationTokens/Controllers/SlowRequestController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CancellationTokens.Controllers
+{
+    public class SlowRequestController : Controller
+    {
+        private readonly ILogger _logger;
+
+        public SlowRequestController(ILogger<SlowRequestController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("/slowtest/{delayTimeInSeconds:int?}")]
+        public async Task<string> Get(CancellationToken cancellationToken,
+            int delayTimeInSeconds = 10)
+        {
+            _logger.LogInformation("Starting to do slow work");
+
+            await Task.Delay(delayTimeInSeconds * 1000, cancellationToken);
+
+            var message = $"Finished slow delay of {delayTimeInSeconds} seconds.";
+
+            for (var i = 0; i < delayTimeInSeconds; i++)
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+                Thread.Sleep(1000);
+            }
+
+            _logger.LogInformation(message);
+
+            return $"Finished slow delay of {delayTimeInSeconds * 2} seconds.";
+        }
+    }
+}
---
+++ CancellationTokens/CancellationTokens/OperationCancelledExceptionFilter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+
+namespace CancellationTokens
+{
+public class OperationCancelledExceptionFilter : ExceptionFilterAttribute
+{
+    private readonly ILogger _logger;
+
+    public OperationCancelledExceptionFilter(ILoggerFactory loggerFactory)
+    {
+        _logger = loggerFactory.CreateLogger<OperationCancelledExceptionFilter>();
+    }
+    public override void OnException(ExceptionContext context)
+    {
+        if(context.Exception is OperationCanceledException)
+        {
+            _logger.LogInformation("Request was cancelled");
+            context.ExceptionHandled = true;
+            context.Result = new StatusCodeResult(400);
+
+        }
+    }
+}
+}
