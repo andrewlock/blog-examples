f8fb4a01c90b5f831c1ba0645870290eeed4dfd9
Andrew Lock <andrewlock.net@gmail.com>
2017-10-29 12:34:45 +0000

    Add example of executable xUnit projecct

---
+++ XUnitWithProgramFile/XUnitWithProgramFile/CriticalTests.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace XUnitWithProgramFile
+{
+    public class CriticalTests
+    {
+        [Fact]
+        public void MyIntegrationTest()
+        {
+            // Do something
+            Console.WriteLine("Testing complete");
+        }
+    }
+}
