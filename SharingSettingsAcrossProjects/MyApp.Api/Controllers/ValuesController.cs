7173f3268b6fbb53f9e000224845977fa8b568bd
Andrew Lock <andrewlock.net@gmail.com>
2018-02-07 10:21:01 +1300

    Add sharing settings project
    https://andrewlock.net/sharing-appsettings-json-configuration-files-between-projects-in-asp-net-core/

---
+++ SharingSettingsAcrossProjects/MyApp.Api/Controllers/ValuesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace MyApp.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        readonly IConfiguration _configuration;
+
+        public ValuesController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return _configuration.GetSection("MySection")
+                .AsEnumerable().Select(kvp => $"{kvp.Key}:{kvp.Value}");
+        }
+    }
+}
---
+++ SharingSettingsAcrossProjects/MyApp.Web/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace MyApp.Web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    var env = hostingContext.HostingEnvironment;
+
+                    // find the shared folder in the parent folder
+                    var sharedFolder = Path.Combine(env.ContentRootPath, "..", "Shared");
+
+                    //load the SharedSettings first, so that appsettings.json overrwrites it
+                    config
+                        .AddJsonFile(Path.Combine(sharedFolder, "SharedSettings.json"), optional: true)
+                        .AddJsonFile("appsettings.json", optional: true)
+                        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+                    config.AddEnvironmentVariables();
+                })
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                    logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                    logging.AddConsole();
+                    logging.AddDebug();
+                })
+                .UseDefaultServiceProvider((ctx, opts) =>
+                {
+                    opts.ValidateScopes = ctx.HostingEnvironment.IsDevelopment();
+                })
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
