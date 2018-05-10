23ac1d918088c56d2e521a2f8607a5beeefc910d
Andrew Lock <andrewlock.net@gmail.com>
2018-05-10 19:58:48 +1200

    Add example for VersionNumbers
    https://andrewlock.net/version-vs-versionsuffix-vs-packageversion-what-do-they-all-mean/

---
+++ VersionNumbers/VersionNumbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+//[assembly: AssemblyVersion("1.2.3.4")]
+//[assembly: AssemblyFileVersion("6.6.6.6")]
+//[assembly: AssemblyInformationalVersion("So many numbers!")]
+
+namespace VersionNumbers
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+
+            Console.WriteLine("This app's version numbers:");
+
+            var assemblyVersion = assembly.GetName().Version;
+
+            Console.WriteLine($"AssemblyVersion       {assemblyVersion}");
+
+            var fileVersionInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
+            var fileVersion = fileVersionInfo.FileVersion;
+
+            Console.WriteLine($"FileVersion           {fileVersion}");
+
+            var informationVersion = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
+
+            Console.WriteLine($"InformationalVersion  {informationVersion}");
+        }
+    }
+}
