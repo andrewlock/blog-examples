0a4aa3532ded31531bdb65fccb547ef145feaa76
Andrew Lock <andrewlock.net@gmail.com>
2018-05-14 23:54:54 +1200

    Suppressing console messages

---
+++ suppress-console-messages/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace suppress_console_messages
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+            // CreateWebHostBuilder(args).Build().Run();
+        }
+        
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            new HostBuilder()
+                .ConfigureLogging((context, builder) => builder.AddConsole())
+                .ConfigureServices(services =>
+                {
+                    services.Configure<ConsoleLifetimeOptions>(options =>
+                        options.SuppressStatusMessages = true);
+                    services.AddSingleton<IHostedService, PrintTextToConsoleService>();
+                });
+
+        // public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        //     WebHost.CreateDefaultBuilder(args)
+        //         .UseSetting(WebHostDefaults.SuppressStatusMessagesKey, "True")
+        //         .UseStartup<Startup>();
+
+
+    }
+}
