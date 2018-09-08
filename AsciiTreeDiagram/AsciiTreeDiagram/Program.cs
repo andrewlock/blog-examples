9b1a66f5b667dbc78e7c59b13a4c635952f806a3
Andrew Lock <andrewlock.net@gmail.com>
2018-09-08 08:03:22 +0100

    Improve ASCII tree

--- AsciiTreeDiagram/AsciiTreeDiagram/Program.cs
+++ AsciiTreeDiagram/AsciiTreeDiagram/Program.cs
@@ -1,52 +1,79 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace AsciiTreeDiagram
 {
     class Program
     {
-        private const string _cross = "├─";
-        private const string _corner = "└─";
-        private const string _vertical = "│ ";
-        private const string _space = "  ";
+        // Constants for drawing lines and spaces
+        private const string _cross = " ├─";
+        private const string _corner = " └─";
+        private const string _vertical = " │ ";
+        private const string _space = "   ";
 
         static void Main(string[] args)
         {
+            // Get the list of nodes
             List<Node> topLevelNodes = CreateNodeList();
 
             foreach (var node in topLevelNodes)
+            { 
+                PrintNode(node, indent: "");
+            }
+
+            if (Debugger.IsAttached)
             {
-                PrintNode(node, "", isLast: false, isTopLevel: true);
+                Console.WriteLine("Press any key to exit...");
+                Console.Read();
             }
         }
 
-        static void PrintNode(Node node, string indent, bool isLast, bool isTopLevel)
+        static void PrintNode(Node node, string indent)
         {
+            Console.WriteLine(node.Name);
+
+            // Loop through the children recursively, passing in the
+            // indent, and the isLast parameter
+            var numberOfChildren = node.Children.Count;
+            for (var i = 0; i < numberOfChildren; i++)
+            {
+                var child = node.Children[i];
+                var isLast = (i == (numberOfChildren - 1));
+                PrintChildNode(child, indent, isLast);
+            }
+        }
+
+        static void PrintChildNode(Node node, string indent, bool isLast)
+        {
+            // Print the provided pipes/spaces indent
             Console.Write(indent);
+
+            // Depending if this node is a last child, print the
+            // corner or cross, and calculate the indent that will
+            // be passed to its children
             if (isLast)
             {
                 Console.Write(_corner);
                 indent += _space;
             }
-            else if (!isTopLevel)
+            else
             {
                 Console.Write(_cross);
                 indent += _vertical;
             }
 
-            Console.WriteLine(node.Name);
-
-            for (var i = 0; i < node.Children.Count; i++)
-            {
-                var child = node.Children[i];
-                PrintNode(child, indent, isLast: i == (node.Children.Count - 1), isTopLevel: false);
-            }
-
+            PrintNode(node, indent);
         }
 
         static void PrintNodeIncorrectly(Node node, int depth, bool isFirstChild, bool isLastChild, bool parentIsLastChild)
         {
             // something
+        }
+
+        static void PrintNodeButMakeHardWorkOfIt(Node node, bool isFirstChild, bool isLastChild, Node[] parentNodes)
+        {
+            // implementation
         }
 
         static List<Node> CreateNodeList()
@@ -208,6 +235,5 @@ namespace AsciiTreeDiagram
                 }
             };
         }
-
     }
 }
