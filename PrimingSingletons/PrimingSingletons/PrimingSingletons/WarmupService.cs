bf9da19db2867cbf371f74299148f17e1f82ad09
Andrew Lock <andrewlock.net@gmail.com>
2019-01-18 18:55:13 +0000

    Add template for priming singletons (and other services)
    (see https://andrewlock.net/reducing-latency-by-pre-building-singletons-in-asp-net-core/)

---
+++ PrimingSingletons/PrimingSingletons/PrimingSingletons/WarmupService.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace PrimingSingletons
+{
+    public class WarmupService : IHostedService
+    {
+        private readonly IServiceCollection _services;
+        private readonly IServiceProvider _provider;
+
+        public WarmupService(
+            IServiceCollection services, 
+            IServiceProvider provider)
+        {
+            _services = services;
+            _provider = provider;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            InstantiteSingletons();
+            InstantiteServices();
+            return Task.CompletedTask;
+        }
+
+        private void InstantiteSingletons()
+        {
+            foreach (var singleton in GetSingletons(_services))
+            {
+                //_logger.LogDebug("Warming up {TypeName}", singleton.FullName);
+                // may be registered more than once, so get all at once
+                _provider.GetServices(singleton);
+            }
+        }
+        private void InstantiteServices()
+        {
+            using (var scope = _provider.CreateScope())
+            {
+                foreach (var service in GetServices(_services))
+                {
+                    //_logger.LogDebug("Warming up {TypeName}", singleton.FullName);
+                    // may be registered more than once, so get all at once
+                    scope.ServiceProvider.GetServices(service);
+                }
+            }
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+
+        static IEnumerable<Type> GetSingletons(IServiceCollection services)
+        {
+            return services
+                .Where(descriptor => descriptor.Lifetime == ServiceLifetime.Singleton
+                                     && descriptor.ImplementationType != typeof(WarmupService))
+                .Distinct()
+                .Where(descriptor => descriptor.ServiceType.ContainsGenericParameters == false)
+                .Select(descriptor => descriptor.ServiceType);
+        }
+
+        static IEnumerable<Type> GetServices(IServiceCollection services)
+        {
+            return services
+                .Where(descriptor => descriptor.ImplementationType != typeof(WarmupService))
+                .Distinct()
+                .Where(descriptor => descriptor.ServiceType.ContainsGenericParameters == false)
+                .Select(descriptor => descriptor.ServiceType);
+        }
+    }
+}
---
+++ PrimingSingletons/PrimingSingletons/PrimingSingletons/WarmupServiceExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Hosting;
+
+namespace PrimingSingletons
+{
+    public static class WarmupServiceExtensions
+    {
+        public static IServiceCollection AddWarmupService(this IServiceCollection services)
+        {
+            services
+                .AddHostedService<WarmupService>()
+                .TryAddSingleton(services);
+
+            return services;
+        }
+    }
+}
