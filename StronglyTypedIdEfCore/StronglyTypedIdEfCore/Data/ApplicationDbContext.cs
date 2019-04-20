5e136e9b370ad672858da8f94a6c477af455a7f2
Andrew Lock <andrewlock.net@gmail.com>
2019-04-20 22:23:04 +0100

    Add initial app code

---
+++ StronglyTypedIdEfCore/StronglyTypedIdEfCore/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using StronglyTypedId.Shop.Data;
+
+namespace StronglyTypedIdEfCore.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
