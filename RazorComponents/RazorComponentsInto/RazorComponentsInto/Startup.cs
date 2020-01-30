245489378b4c5df09fc67bee9e1a125349d83e37
Andrew Lock <andrewlock.net@gmail.com>
2020-01-30 20:52:47 +0000

    Replace LoginPartial with LoginDisplay component

--- RazorComponents/RazorComponentsInto/RazorComponentsInto/Startup.cs
+++ RazorComponents/RazorComponentsInto/RazorComponentsInto/Startup.cs
@@ -33,6 +33,7 @@ namespace RazorComponentsInto
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddRazorPages();
+            services.AddServerSideBlazor();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
