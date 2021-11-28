7f928c05ea6b2a70e22e69b090975656456c68b1
Andrew Lock <andrewlock.net@gmail.com>
2021-11-28 15:52:51 +0000

    Source generator

---
+++ NetEscapades.EnumGenerators/src/NetEscapades.EnumGenerators/SourceGenerationHelper.cs
@@ -0,0 +1,48 @@
+using System.Text;
+namespace NetEscapades.EnumGenerators;
+
+public static class SourceGenerationHelper
+{
+    public const string Attribute = @"
+
+namespace NetEscapades.EnumGenerators
+{
+    [System.AttributeUsage(System.AttributeTargets.Enum)]
+    public class EnumExtensionsAttribute : System.Attribute
+    {
+    }
+}";
+    public static string GenerateExtensionClass(List<EnumToGenerate> enumsToGenerate)
+    {
+        var sb = new StringBuilder();
+        sb.Append(@"
+namespace NetEscapades.EnumGenerators
+{
+    public static partial class EnumExtensions
+    {");
+        foreach(var enumToGenerate in enumsToGenerate)
+        {
+            sb.Append(@"
+                public static string ToStringFast(this ").Append(enumToGenerate.Name).Append(@" value)
+                    => value switch
+                    {");
+            foreach (var member in enumToGenerate.Values)
+            {
+                sb.Append(@"
+                    ").Append(enumToGenerate.Name).Append('.').Append(member)
+                    .Append(" => nameof(")
+                    .Append(enumToGenerate.Name).Append('.').Append(member).Append("),");
+            }
+
+            sb.Append(@"
+                    _ => value.ToString(),
+                };
+");
+        }
+            sb.Append(@"
+    }
+}");
+
+        return sb.ToString();
+    }
+}
