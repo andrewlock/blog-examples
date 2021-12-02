5c06692600655563809e37faf9dbc47d0d1a7f43
Andrew Lock <andrewlock.net@gmail.com>
2021-12-02 22:26:02 +0000

    Snapshot testing

---
+++ NetEscapades.EnumGenerators2/tests/NetEscapades.EnumGenerators.Tests/EnumGeneratorSnapshotTests.cs
@@ -0,0 +1,104 @@
+using VerifyXunit;
+using Xunit;
+
+namespace NetEscapades.EnumGenerators.Tests;
+
+[UsesVerify]
+public class EnumGeneratorSnapshotTests
+{
+    [Fact]
+    public Task GeneratesEnumExtensionsCorrectly()
+    {
+        var source = @"
+using NetEscapades.EnumGenerators;
+
+[EnumExtensions]
+public enum Colour // Yes, I'm British
+{
+    Red = 0,
+    Blue = 1,
+}";
+
+        return TestHelper.Verify(source);
+    }
+
+    [Fact]
+    public Task NoAttribute()
+    {
+        var source = @"
+public enum Colour // Yes, I'm British
+{
+    Red = 0,
+    Blue = 1,
+}";
+
+        return TestHelper.Verify(source);
+    }
+
+    [Fact]
+    public Task NoNamespace()
+    {
+        var source = @"
+[EnumExtensions]
+public enum Colour // Yes, I'm British
+{
+    Red = 0,
+    Blue = 1,
+}";
+
+        return TestHelper.Verify(source);
+    }
+
+    [Fact]
+    public Task TwoEnums()
+    {
+        var source = @"
+using NetEscapades.EnumGenerators;
+
+namespace MyTestEnums;
+
+[EnumExtensions]
+public enum Colour
+{
+    Red = 0,
+    Blue = 1,
+}
+
+[EnumExtensions]
+public enum Direction
+{
+    Left,
+    Right,
+    Up,
+    Down,
+}";
+
+        return TestHelper.Verify(source);
+    }
+
+    [Fact]
+    public Task TwoEnums_OneAttribute()
+    {
+        var source = @"
+using NetEscapades.EnumGenerators;
+
+namespace MyTestEnums;
+
+public enum Colour
+{
+    Red = 0,
+    Blue = 1,
+}
+
+[EnumExtensions]
+public enum Direction
+{
+    Left,
+    Right,
+    Up,
+    Down,
+}";
+
+        return TestHelper.Verify(source);
+    }
+}
---
+++ NetEscapades.EnumGenerators2/tests/NetEscapades.EnumGenerators.Tests/Snapshots/EnumGeneratorSnapshotTests.TwoEnums_OneAttribute.02.verified.cs
@@ -0,0 +1,18 @@
+﻿//HintName: EnumExtensions.g.cs
+
+namespace NetEscapades.EnumGenerators
+{
+    public static partial class EnumExtensions
+    {
+        public static string ToStringFast(this MyTestEnums.Direction value)
+            => value switch
+            {
+                MyTestEnums.Direction.Left => nameof(MyTestEnums.Direction.Left),
+                MyTestEnums.Direction.Right => nameof(MyTestEnums.Direction.Right),
+                MyTestEnums.Direction.Up => nameof(MyTestEnums.Direction.Up),
+                MyTestEnums.Direction.Down => nameof(MyTestEnums.Direction.Down),
+                _ => value.ToString(),
+            };
+
+    }
+}
